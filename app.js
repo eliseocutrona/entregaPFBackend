05a7245b158855717079d01d487c997e10009c1c
eliseocutrona <eliseocutrona@gmail.com>
2024-05-18 15:26:25 -0300

    correciones

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import handlebars from 'express-handlebars';
+import { engine } from 'express-handlebars'; 
+import path from 'path';
 import productRouter from './src/routes/productRouter.js';
 import cartRouter from './src/routes/cartRouter.js';
 import viewsRouter from './src/routes/viewsRouter.js';
@@ -7,22 +8,28 @@ import __dirname from './src/utils/constantsUtil.js';
 
 const app = express();
 
-//Handlebars Config
-app.engine('handlebars', handlebars.engine());
-app.set('views', __dirname + '/../views');
+// Configuración de Handlebars
+app.engine('handlebars', engine()); // Asegúrate de usar 'engine' de 'express-handlebars'
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '../views')); // Usar path.join para construir la ruta
 
-//Middlewares
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public'))); // Usar path.join para construir la ruta
 
-//Routers
-app.use('/api/products', productRouter);
-app.use('/api/carts', cartRouter);
+// Routers
 app.use('/products', viewsRouter);
+app.use('/api/carts', cartRouter);
+app.use('/api/products', productRouter);
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Start server in PORT ${PORT}`);
+  console.log(`Server started on PORT ${PORT}`);
 });
